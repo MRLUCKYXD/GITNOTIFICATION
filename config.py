4a8662b0e8bc23a02e27dc3f1fad775a8ed724c5
ɪᴛ's ᴍᴇ ᴀʙɪsʜɴᴏɪ <106365712+KingAbishnoi@users.noreply.github.com>
2022-07-12 17:01:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Your Bot token in the quotes taken from @BotFather on Telegram
 BOT_TOKEN = "5020504303:AAElG7ieBhOGHDJOgO3Ky0qq8ssTRAeWDTo"
-
+#add your 
 # Project Name of your Project
 PROJECT_NAME = "GitGram"
-
+#dont change 
 # If you forked the Repository, grab your repo's link and paste it below!
 GIT_REPO_URL = "https://github.com/AnonymousBoy1025/GitGram"
