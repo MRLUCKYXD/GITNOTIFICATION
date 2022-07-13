18bdbcee80ba0f41b72daa6db728818ab10eb509
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>
2022-07-13 09:38:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Your Bot token in the quotes from @BotFather
-BOT_TOKEN = "" 
+BOT_TOKEN = "5306400397:AAFMP4MovOd9Ri8534GBcUVGQobEwU5QMvI" 
 # Project Name of your Project
 PROJECT_NAME = "GitAlerts" 
 # If you forked the source code, grab your repo's link and paste it below!
